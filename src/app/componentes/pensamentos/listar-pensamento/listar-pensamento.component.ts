4b26c72b7097660b6014657742d9d6286e4a42b9
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-14 19:30:52 -0300

    feat: :sparkles: usando a diretiva ngFor para gerar uma lista de pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+    conteudo: 'Meu primeiro pensamento',
+    autoria: 'Saulo',
+    modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo3'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
