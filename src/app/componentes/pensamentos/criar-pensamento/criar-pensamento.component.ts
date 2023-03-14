7134a1ea89064879d722c7ca251fb044bfed46bf
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-14 12:17:27 -0300

    feat: :sparkles: utilizando o property binding para trazer dados ao formulario

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Pensamento 1',
+    autoria: 'Autor 1',
+    modelo: '',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
