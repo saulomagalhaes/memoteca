628bb5672163b22e1266342ffae16f9a2bfc9e48
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-15 09:42:09 -0300

    feat: :sparkles: cria um servico para listar os pensamentos

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor(private http:HttpClient) { }
+  private readonly API = "http://localhost:3000/pensamentos"
 
+  constructor(private http: HttpClient) { }
+
+  listar():Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
 
 }
